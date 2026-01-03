3479c75651f0239f28fced0e211807e55db1397e
simps4simps <savarimani6@gmail.com>
2026-01-03 04:17:52 +0330

    Added NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import NavBar from "./_components/NavBar";
+
 export default function Home() {
-  return <div><h1>Hello world!</h1></div>
-} 
+  return (
+    <div
+      className="w-screen h-screen overflow-hidden antialiased
+        flex flex-col"
+    >
+      
+      <NavBar />
+      <div className="flex-1"><h1>Hello world!</h1></div>
+    </div>
+  );
+}
